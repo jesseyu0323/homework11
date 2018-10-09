ad9958b34ac09c942fd7e4802265e3cf290d7bfe
jesseyu0323 <jesseyu.nwds@gmail.com>
2018-10-09 18:38:14 -0500

    update

--- 11/app.py
+++ 11/app.py
@@ -10,12 +10,11 @@ from flask import Flask, jsonify
 #################################################
 # Reflect Tables into SQLAlchemy ORM
 #################################################
-engine = create_engine("sqlite:///Hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
 Base = automap_base()
 # reflect the tables
 Base.prepare(engine, reflect=True)
-
 # Save reference to the table
 Measurement = Base.classes.measurement
 Station = Base.classes.station
@@ -37,7 +36,8 @@ app = Flask(__name__)
 
 #################################################
 year_ago = dt.date(2017, 10, 5) - dt.timedelta(days=365)
-most_active = active_station[0][0]   
+active_station = session.query(Measurement.station, func.count(Measurement.tobs)).group_by(Measurement.station).order_by(func.count(Measurement.tobs).desc()).all()
+most_active = active_station[0][0]
 
 #################################################
 # Flask Routes
@@ -45,6 +45,7 @@ most_active = active_station[0][0]
 @app.route("/")
 def welcome():
     """List all available api routes."""
+      
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
@@ -64,6 +65,7 @@ def precipitation():
 # Return the JSON representation of your dictionary.
     
     year_ago = dt.date(2017, 10, 5) - dt.timedelta(days=365)
+    session = Session(engine)
     score = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date > year_ago).order_by(Measurement.date).all()
     return jsonify(score)
 
@@ -72,6 +74,7 @@ def precipitation():
 def stations():
     """Return a list of station for last year"""
 #  Return a JSON list of stations from the dataset.
+    session = Session(engine)
     active_station = session.query(Measurement.station, func.count(Measurement.tobs)).group_by(Measurement.station).order_by(func.count(Measurement.tobs).desc()).all()
     most_active = active_station[0][0]   
     return jsonify(active_station)
@@ -81,36 +84,33 @@ def stations():
 def tobs():
     """Return a list of temperatures for last year"""
 #  Return a JSON list of Temperature Observations (tobs) for the previous year.
+    session = Session(engine)
     temp_obsv = session.query(Measurement.station, Measurement.date, Measurement.tobs).filter(Measurement.station == most_active).filter(Measurement.date > year_ago).order_by(Measurement.date).all()
     return jsonify(temp_obsv)
 
 #########################################################################################
-@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/average/<start>")
 def startdate(start):
 
  # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range.
-    start_date = dt.datetime.strptime(start, '%Y-%m-%d')
-    last_date = dt.timedelta(days=365)
-    start = start_date-last_date
-    temp_list = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
-        filter(Measurement.date >= start).all()
-    temp = list(np.ravel(temp_list))
-    return jsonify(temp)
+    session = Session(engine)
+ 
+    temp_list = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+    
+    return jsonify(temp_list)
 
 #########################################################################################
-@app.route("/api/v1.0/<start>/<end>")
+@app.route("/api/v1.0/average/<start>/<end>")
 def startdateenddate(start,end):
 
   # Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start or start-end range. 
-    start_date = dt.datetime.strptime(start, '%Y-%m-%d')
-    end_date = dt.datetime.strptime(end,'%Y-%m-%d')
-    last_year = dt.timedelta(days=365)
-    start = start_date-last_year
-    end = end_date-last_year
+    session = Session(engine)
+   
+    
     temp_list = session.query(func.min(Measurement.tobs), func.max(Measurement.tobs), func.avg(Measurement.tobs)).\
         filter(Measurement.date >= start).filter(Measurement.date <= end).all()
-    temp = list(np.ravel(temp_list))
-    return jsonify(temp)
+   
+    return jsonify(temp_list)
 
 
 if __name__ == "__main__":
